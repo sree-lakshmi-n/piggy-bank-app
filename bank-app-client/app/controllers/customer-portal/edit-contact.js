bc4b5a93c97426381cb05e5b0982ecf83e32dc2b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-04-13 14:17:26 +0530

    edit contact details customer functionality

---
+++ bank-app-client/app/controllers/customer-portal/edit-contact.js
@@ -0,0 +1,63 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class CustomerPortalEditContactController extends Controller {
+  @service('auth') auth;
+  @service router;
+  @service('regex-check') regex;
+
+  @tracked isEmailValid = true;
+  @tracked isMobileNumValid = true;
+
+  @action
+  validateFormElements(fieldName, event) {
+    if (fieldName === 'email') {
+      this.isEmailValid = this.regex.get('emailRegex').test(event.target.value);
+      console.log(this.isEmailValid);
+    } else if (fieldName === 'mobileNum') {
+      this.isMobileNumValid = this.regex
+        .get('mobileNumRegex')
+        .test(event.target.value);
+      console.log(this.isMobileNumValid);
+    }
+  }
+
+  @action
+  async editContactDetails(event) {
+    const element = event.target.closest('.form');
+    let email = element.querySelector('.input-email ').value;
+    let mobilenum = element.querySelector('.input-mobile-num').value;
+    if (email == null && mobilenum == null) {
+      alert('Enter valid values');
+    } else if (this.isEmailValid && this.isMobileNumValid) {
+      if (email == null && mobilenum != null) {
+        email = this.auth.get('email');
+      } else if (email != null && mobilenum == null) {
+        mobilenum = this.auth.get('phonenum');
+      }
+      const response = await fetch('http://localhost:8000/update', {
+        method: 'POST',
+        headers: {
+          custid: this.auth.currentCustId,
+          email: email,
+          phonenum: mobilenum,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+      if (response.ok) {
+        const json = await response.json();
+        alert(json.message);
+        this.auth.email = email;
+        this.auth.mobilenum = mobilenum;
+        element.querySelector('.input-email ').value = '';
+        element.querySelector('.input-mobile-num').value = '';
+      } else {
+        const json = await response.json();
+        alert(json.message);
+      }
+    }
+  }
+}
