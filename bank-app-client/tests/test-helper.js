7635ebe928f1b9d8dab8feb5b5ea58672ade6823
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-04-13 13:06:44 +0530

    created ember project

---
+++ bank-app-client/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'bank-app-client/app';
+import config from 'bank-app-client/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
