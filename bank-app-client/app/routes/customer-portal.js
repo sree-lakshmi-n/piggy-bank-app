39ba620aa6f5304a965a12a286fab0ac4f303745
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-04-17 23:57:18 +0530

    sessionId cookie added on login and removed on logout

--- bank-app-client/app/routes/customer-portal.js
+++ bank-app-client/app/routes/customer-portal.js
@@ -15,9 +15,32 @@ export default class CustomerPortalRoute extends Route {
       'logout',
     ];
   }
-  afterModel() {
-    if (this.auth.get('currentCustId') === null) {
+  async afterModel() {
+    if (
+      this.auth.getCookie('sessionId') === null ||
+      this.auth.getCookie('sessionId') === ''
+    ) {
       this.router.transitionTo('index');
+    } else {
+      const sessionId = this.auth.getCookie('sessionId');
+      const response = await fetch('http://localhost:8000/custid', {
+        method: 'POST',
+        headers: {
+          sessionid: sessionId,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+      const json = await response.json();
+      if (response.ok) {
+        console.log(json);
+        this.auth.currentCustId = json.message.split('+')[0];
+        this.auth.accountNum = json.message.split('+')[1];
+        this.auth.upiId = json.message.split('+')[2];
+        await this.auth.getCustomerInfo();
+        await this.auth.getTransactionTable();
+        this.router.transitionTo('customer-portal.profile');
+      }
     }
   }
 }
